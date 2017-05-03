63f9f6e92355e27ea8a5e84048cc2596c1444b89
Jonathan Christison <jochrist@redhat.com>
2017-05-03 17:22:10 +0100

    Openshift changes

--- slim/docker-entrypoint.sh
+++ slim/docker-entrypoint.sh
@@ -18,7 +18,7 @@ cd /
 
 # Make sure $DATADIR is owned by znc user. This affects ownership of the
 # mounted directory on the host machine too.
-chown -R znc:znc "$DATADIR" || exit 1
+chown -R znc:root "$DATADIR" || exit 1
 chmod 700 "$DATADIR" || exit 2
 
 # ZNC itself responds to SIGTERM, and reaps its children, but whatever was
